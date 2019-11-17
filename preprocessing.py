7d55d9dacf4b86dc6a9893bd4c9ab1c78e46b1ef
Michelangelo Rubino <mrubin01@dcs.bbk.ac.uk>
2019-11-17 20:20:24 +0000

    added code for binarising data

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,10 @@ import csv
 import numpy
 import pandas as pd
 from matplotlib import pyplot
+from sklearn.preprocessing import Binarizer
+
+
+''' Load data and create a dataframe '''
 
 # change the path with your local path
 filename = r'C:\Users\user\jupyterSourceFiles\datasets\awid\train_imperson_without4n7_balanced_data.csv'
@@ -12,13 +16,16 @@ dfTrain = pd.read_csv(filename,
                       na_values=['nan'])
 
 
-# see dataframe info
+''' View dataframe info '''
+
 dfTrain.head()
 dfTrain.info()
 dfTrain.describe()
 
 
-# many attributes have no values, this code delete them from the dataframe
+''' Delete columns with no values '''
+
+# many attributes have no values
 columnsToDelete = [] 
 
 for key, value in dfTrain.iteritems():
@@ -35,7 +42,10 @@ dfTrain = dfTrain.drop(columnsToDelete, axis=1)
 class_counts = dfTrain.groupby('155').size()
 class_counts
 
-# show the 10 attributes with the highest positive correlations with the class
+
+''' Sort attributes by correlation and create 4 smaller dataframe '''
+
+# show the 10 attributes with the highest positive correlations
 correlations = dfTrain.corr(method='pearson')
 corr = correlations["155"].sort_values(ascending=False) # sort the correlations from highest to lowest
 corr[0:10]
@@ -52,18 +62,20 @@ LoCorr60 = ['119', '123', '144', '106', '72', '98', '90', '125', '118', '112',
 LoCorr80 = ['130', '126', '107', '94', '122', '77', '110', '70', '75', '154', 
                '9', '8', '145', '146', '82', '47', '51', '67', '155']
 
-
 # create 4 smaller dataframes
 dfTrain20 = dfTrain[HiCorr20]
 dfTrain40 = dfTrain[HiCorr40]
 dfTrain60 = dfTrain[LoCorr60]
 dfTrain80 = dfTrain[LoCorr80]
 
-# analyze the first dataframe
+
+''' Analysis and plotting of the dataframe dfTrain20 '''
+
+# skewness
 skew = dfTrain20.skew()
 skew
 
-# plot the data for dfTrain20
+# plot the data
 # each plot requires a certain amount of time
 
 # density plot
@@ -85,6 +97,28 @@ pyplot.show()
 # scatter_matrix(dfTrain20, figsize=[20, 20])
 # pyplot.show()
 
+
+''' Binarise data '''
+''' Standardisation, Normalisation and PowerTransformer have no effect on the normal form '''
+
+# extract the values from the dataframe
+array = dfTrain20.values
+X = array[:, 0:20]
+Y = array[:, 20] # the class does not need to be binarized
+
+binarizer = Binarizer(threshold=0.5).fit(X)
+binaryX = binarizer.transform(X)
+
+# summarize transformed data
+set_printoptions(precision=3)
+print(binaryX[0:10,:])
+
+# create a new dataframe with the binarized values
+dfTrain20_binarized = pd.DataFrame(binaryX, columns=HiCorr20[0:20])
+
+# add the class to the binarized dataframe
+# the dataframe will have 19 binarized attributes + the class
+dfTrain20_binarized.insert(20, '155', dfTrain20['155'], True)
 
 
 
